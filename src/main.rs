7b4aea29d0a6e18fe0d6e13e14b73efb37a693e7
Luiz Rocco <luizgustavorocco@gmail.com>
2023-06-03 13:28:19 -0300

    Used chatGippity to refactor code and learn more about best practices

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use clap::Parser;
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Cli {
-    path: std::path::PathBuf,
+    path: PathBuf,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
 
-    let csv_file = std::fs::read_to_string(&args.path)?;
+    let csv_file = fs::read_to_string(&args.path)?;
+
+    let mut array_of_lines: Vec<Vec<&str>> = Vec::new();
 
     for line in csv_file.lines() {
-        println!("{}", line);
+        let line_elements: Vec<&str> = line.split(",").collect();
+        array_of_lines.push(line_elements);
     }
+
+    println!("{:?}", array_of_lines);
+
     Ok(())
 }
