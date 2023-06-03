1c4b46b4f25588985ee16ef00bb3b7e8e9e484ae
Luiz Rocco <luizgustavorocco@gmail.com>
2023-06-03 03:13:00 -0300

    Added a sample file for parsing and improved code readability using the ? operator

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
+
+    let csv_file = std::fs::read_to_string(&args.path)?;
+
+    for line in csv_file.lines() {
+        println!("{}", line);
+    }
+    Ok(())
 }
