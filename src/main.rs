0359397cbbe2e0a242b64d8ce43d7ac692d4a98a
Luiz Rocco <luizgustavorocco@gmail.com>
2023-06-06 01:21:31 -0300

    Trying really hard to fight the borrow checker, but unsuccessful

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
+use csv::Reader;
 use std::error::Error;
-use std::fs;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -8,19 +8,98 @@ struct Cli {
     path: PathBuf,
 }
 
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let args = Cli::parse();
+
+//     let mut csv_reader = Reader::from_path(&args.path)?;
+
+//     let headers = csv_reader.headers()?.clone();
+//     let relevant_headers = vec!["Project", "Issue", "Summary", "Time spent"];
+//     let records = csv_reader.records();
+
+//     let relevant_columns: Vec<Vec<String>> = headers
+//         .iter()
+//         .enumerate()
+//         .filter_map(|(_, header)| {
+//             if relevant_headers.contains(&header) {
+//                 Some(header)
+//             } else {
+//                 None
+//             }
+//         })
+//         .map(|(index, _)| {
+//             csv_reader
+//                 .records()
+//                 .map(|record| record.map(|row| row[index].to_owned()))
+//                 .collect::<Result<Vec<_>, _>>()
+//         })
+//         .collect::<Result<Vec<_>, _>>()?;
+
+//     for record in relevant_columns {
+//         println!("{:?}", record);
+//     }
+
+//     Ok(())
+// }
+
+// fn main() -> Result<(), Box<dyn Error>> {
+//     let args = Cli::parse();
+
+//     let mut csv_reader = Reader::from_path(&args.path)?;
+
+//     let headers = csv_reader.headers()?.into_iter();
+//     let relevant_headers = vec!["Project", "Issue", "Summary", "Time spent"];
+
+//     let index_array: Vec<usize> = headers
+//         .enumerate()
+//         .filter(|(_, header)| relevant_headers.contains(header))
+//         .map(|(index, _)| index)
+//         .collect();
+
+//     let relevant_records = csv_reader.records().map(|record| {
+//         record
+//             .into_iter()
+//             .enumerate()
+//             .filter(|(column_index, _)| index_array.contains(column_index))
+//             // .map(|(_, column)| column)
+//             .collect::<Vec<_>>()
+//     });
+
+//     for record in relevant_records {
+//         println!("{:?}", record);
+//     }
+
+//     Ok(())
+// }
+
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
 
-    let csv_file = fs::read_to_string(&args.path)?;
+    let mut csv_reader = Reader::from_path(&args.path)?;
 
-    let mut array_of_lines: Vec<Vec<&str>> = Vec::new();
+    let headers = csv_reader.headers()?.clone();
 
-    for line in csv_file.lines() {
-        let line_elements: Vec<&str> = line.split(",").collect();
-        array_of_lines.push(line_elements);
+    let relevant_headers = vec!["Project", "Issue", "Summary", "Time spent"];
+
+    let relevant_records = csv_reader.records().map(|record| {
+        record
+            .unwrap()
+            .into_iter()
+            .enumerate()
+            .filter_map(|(column_index, column)| {
+                if relevant_headers.contains(&headers.get(column_index)?) {
+                    Some(column)
+                } else {
+                    None
+                }
+            })
+    });
+
+    for record in relevant_records {
+        for column in record {
+            println!("{:?}", column);
+        }
     }
-
-    println!("{:?}", array_of_lines);
 
     Ok(())
 }
