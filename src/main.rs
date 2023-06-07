73d2b13b82014c48c49f1628de30e78a1a355446
Luiz Rocco <luizgustavorocco@gmail.com>
2023-06-07 01:30:26 -0300

    Fixed ownership problem the best way I could for now

--- src/main.rs
+++ src/main.rs
@@ -8,78 +8,13 @@ struct Cli {
     path: PathBuf,
 }
 
-// fn main() -> Result<(), Box<dyn Error>> {
-//     let args = Cli::parse();
-
-//     let mut csv_reader = Reader::from_path(&args.path)?;
-
-//     let headers = csv_reader.headers()?.clone();
-//     let relevant_headers = vec!["Project", "Issue", "Summary", "Time spent"];
-//     let records = csv_reader.records();
-
-//     let relevant_columns: Vec<Vec<String>> = headers
-//         .iter()
-//         .enumerate()
-//         .filter_map(|(_, header)| {
-//             if relevant_headers.contains(&header) {
-//                 Some(header)
-//             } else {
-//                 None
-//             }
-//         })
-//         .map(|(index, _)| {
-//             csv_reader
-//                 .records()
-//                 .map(|record| record.map(|row| row[index].to_owned()))
-//                 .collect::<Result<Vec<_>, _>>()
-//         })
-//         .collect::<Result<Vec<_>, _>>()?;
-
-//     for record in relevant_columns {
-//         println!("{:?}", record);
-//     }
-
-//     Ok(())
-// }
-
-// fn main() -> Result<(), Box<dyn Error>> {
-//     let args = Cli::parse();
-
-//     let mut csv_reader = Reader::from_path(&args.path)?;
-
-//     let headers = csv_reader.headers()?.into_iter();
-//     let relevant_headers = vec!["Project", "Issue", "Summary", "Time spent"];
-
-//     let index_array: Vec<usize> = headers
-//         .enumerate()
-//         .filter(|(_, header)| relevant_headers.contains(header))
-//         .map(|(index, _)| index)
-//         .collect();
-
-//     let relevant_records = csv_reader.records().map(|record| {
-//         record
-//             .into_iter()
-//             .enumerate()
-//             .filter(|(column_index, _)| index_array.contains(column_index))
-//             // .map(|(_, column)| column)
-//             .collect::<Vec<_>>()
-//     });
-
-//     for record in relevant_records {
-//         println!("{:?}", record);
-//     }
-
-//     Ok(())
-// }
-
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
 
     let mut csv_reader = Reader::from_path(&args.path)?;
 
     let headers = csv_reader.headers()?.clone();
-
-    let relevant_headers = vec!["Project", "Issue", "Summary", "Time spent"];
+    let relevant_headers = ["Project", "Issue", "Summary", "Time spent", "Started"];
 
     let relevant_records = csv_reader.records().map(|record| {
         record
@@ -93,12 +28,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                     None
                 }
             })
+            .map(|column| column.to_owned())
+            .collect::<Vec<_>>()
     });
 
     for record in relevant_records {
-        for column in record {
-            println!("{:?}", column);
-        }
+        // for column in record {
+        println!("{:?}", record);
+        // }
     }
 
     Ok(())
