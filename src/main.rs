0c20706b0a862ef875a5349f445ba4c792955953
Luiz Rocco <luizgustavorocco@gmail.com>
2023-06-14 01:41:12 -0300

    More progress towards the goal, the code feels ugly as hell though.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDate;
 use clap::Parser;
 use csv::Reader;
 use std::error::Error;
@@ -16,30 +17,60 @@ fn main() -> Result<(), Box<dyn Error>> {
     let headers = csv_reader.headers()?.clone();
     let relevant_headers = ["Project", "Issue", "Summary", "Time spent", "Started"];
 
-    let records = csv_reader
+    let mut records = csv_reader
         .into_records()
         .filter_map(|record| record.ok())
         .collect::<Vec<_>>();
 
-    let relevant_records = records.iter().map(|record| {
-        record
-            .into_iter()
-            .enumerate()
-            .filter_map(|(column_index, column)| {
-                if relevant_headers.contains(&headers.get(column_index)?) {
-                    Some(column)
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>()
-    });
+    let total = records.pop().unwrap();
+
+    let relevant_records = records
+        .iter()
+        .map(|record| {
+            record
+                .into_iter()
+                .enumerate()
+                .filter_map(|(column_index, column)| {
+                    if relevant_headers.contains(&headers.get(column_index)?) {
+                        Some(column)
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        })
+        .map(|mut row| {
+            let date_column = &row[3];
+
+            let date_arr = date_column.split(",").collect::<Vec<_>>();
+
+            let month_day = date_arr[0];
+            let year = date_arr[1];
+
+            let month_day = month_day.split(" ").collect::<Vec<_>>();
+
+            let month = month_day[0];
+            let padded_day = if month_day[1].len() == 1 {
+                format!("0{}", month_day[1])
+            } else {
+                month_day[1].to_owned()
+            };
+
+            let stringified_date = format!("{}-{}-{}", month, padded_day, year);
+
+            let date = NaiveDate::parse_from_str(&stringified_date, "%B-%d- %Y")
+                .unwrap()
+                .to_string();
+
+            println!("{:?}", date);
+
+            row[3] = Box::leak(date.into_boxed_str());
+            row
+        })
+        .collect::<Vec<_>>();
 
     for record in relevant_records {
-        let date_column = &record[3];
-
-        let day = date_column.split(",").collect::<Vec<_>>();
-        println!("{:?}", day);
+        println!("{:?}", record);
     }
 
     Ok(())
